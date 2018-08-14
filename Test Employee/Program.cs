18fc16264e9b21dc6998f26acd668dea64175086
Your Name <you@example.com>
2018-08-14 18:31:03 +1200

    first commit

---
+++ Test Employee/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open chrome browser
+            IWebDriver driver = new ChromeDriver(@"C:\Users\SONY\Downloads\chromedriver_win32");
+
+            //Go to the url
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+        }
+    }
+}
